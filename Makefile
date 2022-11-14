4471b77f0bc26a7933d2563986175298df4615d3
Chia-Che Tsai <chiache@tamu.edu>
2022-11-14 07:03:58 -0600

    Introducing pthread

--- Makefile
+++ Makefile
@@ -1,3 +1,4 @@
+LDFLAGS += -pthread
 uthread: uthread.o main.o
 uthread.o: uthread.h
 main.o: uthread.h
--- uthread.cpp
+++ uthread.cpp
@@ -1,5 +1,6 @@
 /* Write your core functionality in this file. */
 
+#include <pthread.h>
 #include "uthread.h"
 
 void uthread_init(void)
@@ -15,5 +16,9 @@ void uthread_exit(void)
 }
 
 void uthread_yield(void)
+{
+}
+
+void uthread_cleanup(void)
 {
 }
